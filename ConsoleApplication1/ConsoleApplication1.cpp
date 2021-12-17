23a5cd19ee8c4375cfd86da92043f34b40392e26
sidha <sidha@DESKTOP-FJ5P48O>
2021-12-16 19:39:30 -0700

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "Calculator.h"
+
+using namespace std;
+
+int main()
+{
+    double x = 0.0;
+    double y = 0.0;
+    double result = 0.0;
+    char oper = '+';
+
+    cout << "Calculator Console Application" << endl << endl;
+    cout << "By:Siddharth Damodharan and Greysen Bahl" << endl;
+    cout << "Suck it Green" << endl;
+    cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b"
+        << endl;
+
+    Calculator c;
+    while (true)
+    {
+        cin >> x >> oper >> y;
+        result = c.Calculate(x, oper, y);
+        cout << "Result is: " << result << endl;
+    }
+
+    return 0;
+}
